91cc7ece68c3fc488ea69054d8a054f0d03c899f
xanderyeng <ckalex54@gmail.com>
2025-10-06 15:33:46 +0300

    chore: footer social links conflict fix

--- components/wrapper/footer/social-links.tsx
+++ components/wrapper/footer/social-links.tsx
@@ -1,30 +1,34 @@
 import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
+import Link from 'next/link';
 
 const socialLinks = [
-  { icon: Facebook, href: '#', className: 'hover:bg-blue-600' },
-  { icon: Twitter, href: '#', className: 'hover:bg-blue-400' },
+  { icon: Facebook, href: 'https://facebook.com', className: 'hover:bg-blue-600' },
+  { icon: Twitter, href: 'https://x.com', className: 'hover:bg-blue-400' },
   {
     icon: Instagram,
     href: '#',
     className: 'bg-[var(--color-secondary-pink-50)] hover:bg-[var(--color-secondary-pink-60)]',
   },
-  { icon: Linkedin, href: '#', className: 'hover:bg-blue-700' },
+  {
+    icon: Linkedin,
+    href: 'https://www.linkedin.com/company/nmcyber',
+    className: 'hover:bg-blue-700',
+  },
 ];
 
 export function SocialLinks() {
   return (
     <div className="flex gap-3 pt-4">
-      {socialLinks.map((social,idx) => {
+      {socialLinks.map((social) => {
         const Icon = social.icon;
-        return ( // replace 'key' with 'idx' as key requires unique requirement
-          <a
-          key={`${social.label ?? 'social'}-${idx}`}
-          href={social.href}
-          className={`w-8 h-8 bg-secondary rounded-md flex items-center justify-center transition-colors ${social.className}`}
-          aria-label={social.label ?? 'social link'}
-        >
-          <Icon className="w-4 h-4" />
-        </a>
+        return (
+          <Link
+            key={social.href}
+            href={social.href}
+            className={`w-8 h-8 bg-secondary rounded-md flex items-center justify-center transition-colors ${social.className}`}
+          >
+            <Icon className="w-4 aspect-square text-white" />
+          </Link>
         );
       })}
     </div>
