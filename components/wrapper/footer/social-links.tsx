4c3fb54deed87e72b940569d5edaca44e8177e9c
xanderyeng <ckalex54@gmail.com>
2025-10-06 15:28:01 +0300

    chore: keys fixing

--- components/wrapper/footer/social-links.tsx
+++ components/wrapper/footer/social-links.tsx
@@ -1,15 +1,15 @@
 import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
+import Link from 'next/link';
 
 const socialLinks = [
-  { icon: Facebook, href: '#', className: 'hover:bg-blue-600' },
-  { icon: Twitter, href: '#', className: 'hover:bg-blue-400' },
+  { icon: Facebook, href: 'https://facebook.com', className: 'hover:bg-blue-600' },
+  { icon: Twitter, href: 'https://x.com', className: 'hover:bg-blue-400' },
   {
     icon: Instagram,
     href: '#',
     className: 'bg-[var(--color-secondary-pink-50)] hover:bg-[var(--color-secondary-pink-60)]',
   },
-  { icon: Linkedin, href: '#', className: 'hover:bg-blue-700' },
-];
+  { icon: Linkedin, href: 'https://www.linkedin.com/company/nmcyber', className: 'hover:bg-blue-700' },];
 
 export function SocialLinks() {
   return (
@@ -17,13 +17,13 @@ export function SocialLinks() {
       {socialLinks.map((social) => {
         const Icon = social.icon;
         return (
-          <a
+          <Link
             key={social.href}
             href={social.href}
             className={`w-8 h-8 bg-secondary rounded-md flex items-center justify-center transition-colors ${social.className}`}
           >
-            <Icon className="w-4 h-4 text-white" />
-          </a>
+            <Icon className="w-4 aspect-square text-white" />
+          </Link>
         );
       })}
     </div>
